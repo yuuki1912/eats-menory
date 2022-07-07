d70d150c9ae689a351974eabcdafce1167adfd63
yuuki1912 <108678166+yuuki1912@users.noreply.github.com>
2022-07-07 09:20:41 +0900

    Create edit.blade.php

---
+++ views/edit.blade.php
@@ -0,0 +1,110 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>新規投稿ページ</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <p class="navbar-brand">
+                    Eat Memory
+                </p>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">マイページ</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">新規登録</a>
+                                </li>
+                            @endif
+                        @else
+                        <li class="nav-item"><a class="nav-link" href="{{ route('eats.index') }}">メインページ</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('eats.myPage', ['user_id' => Auth::id()]) }}">マイページ</a></li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                        ログアウト
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <main class="py-4">
+        <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form action="{{ route('eats.update', $post->id) }}" method="POST">
+            {{csrf_field()}}
+            {{method_field('PATCH')}}
+            <h1>編集内容</h1>
+                <div class="form-group">
+                    <label>タイトル</label>
+                    <input type="text" class="form-control" placeholder="タイトルを入力して下さい" name="title" value="{{ $post->title }}">
+                </div>
+                <div class="form-group">
+                    <label>内容</label>
+                    <textarea class="form-control" placeholder="内容" rows="5" name="body">{{ $post->body }}</textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">更新</button>
+                <a href=" {{ route('eats.show', $post->id) }} " class="btn btn-danger" id="editBtn">詳細画面へ戻る</a>
+            </form>
+        </div>
+    </div>
+        </main>
+    </div>
+</body>
+</html>
